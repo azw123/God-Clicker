f4612862f0e4be85bf1803e3f7a41113fa6df462
azw123 <59127788+azw123@users.noreply.github.com>
2021-01-28 17:34:33 -0800

    Automation

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+//define variables
 var currency = 0;
+var player = {
+    dryads: {
+        count: 0,
+        price: 10,
+        currencyGenerated: 10
+    },
+    naiads: {
+        count : 0,
+        price: 100,
+        currencyGenerated: 100
+    }
+}
+//base gameplay
+
 $("#counter").hide();
 function gp(amount){
     currency+=amount;
@@ -7,3 +22,18 @@ function gp(amount){
 $( "#base-button" ).click(function() {
     $("#counter").fadeIn(500);
 });
+
+//add something to a variables
+function add(name){
+    name = JSON.parse(name);
+    if (name.price >= currency){
+        currency -= name.price;
+        name.price *= 1.1;
+        name.count += 1;
+    }
+}
+//automatic stuff
+for (i = 0; i<player.length; i++){
+    currency += player[i].currencyGenerated * player[i].count;
+}
+
