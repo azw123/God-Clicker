9d415e1f033a86fdcc6108edde6037375d6a4a78
azw123 <59127788+azw123@users.noreply.github.com>
2021-05-09 16:57:18 -0700

    Just some features adding

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1)
+}
 //define variables
 var currency = 0;
 var player = {
@@ -14,14 +17,18 @@ var player = {
 }
 
 //hide and show
-$("#counter").hide();
 $(".whole").hide();
 $("#auto1").show();
-if(currency>50) $("#auto2").fadeIn(500);
+$("#auto2").hide();
+function show(){
+    if(currency>50){ $("#auto2").fadeIn(500);}
+}
+show();
 //base gameplay
 function gp(amount){
     currency+=amount;
     $("#counter").text("God Points: " + currency)
+    show();
 }
 $( "#base-button" ).click(function() {
     $("#counter").fadeIn(500);
@@ -29,15 +36,32 @@ $( "#base-button" ).click(function() {
 
 //add something to a variables
 function add(name){
-    name = JSON.parse(name);
-    if (name.price >= currency){
-        currency -= name.price;
-        name.price *= 1.1;
-        name.count += 1;
+    if (player[name].price <= currency){
+        currency -= Math.round(player[name].price);
+        player[name].price *= 1.1;
+        player[name].price = Math.round(player[name].price)
+        player[name].count += 1;
+        $("#counter").text("God Points: "+currency);
+        $("#"+name.capitalize()).html("Price: "+ player[name].price + "<br>" + name.capitalize()+": "+player[name].count);
+
+    }else{
+        if($("#"+name.capitalize()).get(0).innerHTML.indexOf("Not enough God Points") == -1){
+            $("#"+name.capitalize()).append("<br> Not enough God Points");
+        }
+    }
+    for (let i = 0; i < $("button").length; i++) {
+        if($("button")[i].innerText == player[0]) {
+            console.log($("button")[i].innerText)
+        }
     }
 }
 //automatic stuff
-for (i = 0; i<player.length; i++){
-    currency += player[i].currencyGenerated * player[i].count;
-}
-
+setInterval(function(){    
+    for (var key in player) {
+        if (player.hasOwnProperty(key)) {
+            currency += 1/10*(player[key].currencyGenerated*player[key].count);
+            $("#counter").text("God Points: "+currency);
+            show();
+        }
+    }
+},100)
